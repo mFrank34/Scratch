4e3853782273f7624459026582851103480b25ae
Michael Franks <mffranks34@gmail.com>
2026-04-17 18:22:25 +0100

    fixing CMakeLists.txt

--- src/io/IOHandler.cpp
+++ src/io/IOHandler.cpp
@@ -5,10 +5,11 @@
  */
  
 #include "IOHandler.h"
+#include <utils/Log.h>
 
 bool IOHandler::write(TextDocument& data)
 {
-
+    Log::info("Hello World!");
 }
 
 TextDocument IOHandler::read(std::string path)
